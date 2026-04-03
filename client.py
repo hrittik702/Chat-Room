32bd0019b64dfc5beeb7eb767132510e220f6e6f
hrittik702 <mmhrittik@gmail.com>
2026-04-03 11:22:04 +0530

    Test file : v1

--- client.py
+++ client.py
@@ -4,7 +4,7 @@ import tkinter as tk
 from tkinter import scrolledtext, messagebox
 import time
 
-HOST = '127.0.0.1'
+HOST = '192.168.226.102'
 PORT = 12345
 
 
--- server1.py
+++ server1.py
@@ -1,7 +1,7 @@
 import socket
 import threading
 
-HOST = '127.0.0.1'
+HOST = '192.168.226.102'
 PORT = 12345
 
 # {room_code: [(client_socket, username), ...]}
